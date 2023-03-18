0b7870b173a8cc55d3bb819cd2aa1ac6f61d32a2
Brandon Butler <brandon@brandonbutler.me>
2023-03-18 19:09:58 -0400

    add smoke test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,23 +8,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:quest_phase/main.dart';
+import 'package:quest_phase/pages/new_game_page/new_game_page.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const QuestPhase());
+  Widget createPageForTesting({required Widget child}) {
+    return MaterialApp(
+      home: child,
+    );
+  }
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+  String? fromRichTextToPlainText(Widget widget) {
+    if (widget is RichText) {
+      return widget.text.toPlainText();
+    }
+    return null;
+  }
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('welcome smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(createPageForTesting(child: const NewGamePage()));
+    expect(
+      find.byWidgetPredicate(
+        (widget) => fromRichTextToPlainText(widget) == 'WELCOME',
+      ),
+      findsOneWidget,
+    );
   });
 }
